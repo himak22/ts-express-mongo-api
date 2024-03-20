dcc4caaeed53492aa2276186045379acdbce885b
Andres Gonzalez Espinosa <andres@Andress-MBP.lan>
2024-03-20 22:38:32 +0100

    Update and delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers } from "../db/users";
+import { getUsers, deleteUserById, getUserById } from "../db/users";
 
 export const getAllUsers = async (
   req: express.Request,
@@ -9,6 +9,48 @@ export const getAllUsers = async (
   try {
     const users = await getUsers();
     return res.status(200).json(users).end();
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    // Delete the user
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser).end();
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(400);
+  }
+};
+
+export const updateUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    // Update the user
+    const { id } = req.params;
+    const { username } = req.body;
+
+    if (!username) {
+      return res.sendStatus(400);
+    }
+
+    const user = await getUserById(id);
+
+    user.username = username;
+    await user.save();
+
+    return res.status(200).json(user).end();
   } catch (error) {
     console.error(error);
     return res.sendStatus(400);
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -3,6 +3,31 @@ import { get, merge } from "lodash";
 
 import { getUserBySessionToken } from "../db/users";
 
+export const isOwner = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  // Middleware function to check if a user is the owner of a resource
+  try {
+    const { id } = req.params; // Get the ID from the request parameters
+    const currentUser = get(req, "identity._id") as string; // Get the current user's ID from the request object
+
+    if (!currentUser) {
+      return res.sendStatus(403); // Unauthorized
+    }
+
+    if (currentUser.toString() !== id) {
+      return res.sendStatus(403); // Unauthorized
+    }
+
+    next(); // Continue
+  } catch (error) {
+    console.error(error);
+    return res.sendStatus(400); // Bad request
+  }
+};
+
 export const isAuthenticated = async (
   req: express.Request,
   res: express.Response,
